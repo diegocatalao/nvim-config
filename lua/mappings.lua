a4f1812258c26bfb8971ab5a586ef8063bc93f39
Diego Catal <diegohutter@icloud.com>
2025-12-14 22:29:41 -0300

    feat: a snippet to open diffview

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,16 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
+
+map("n", "<leader>gd", function()
+  local ok, lib = pcall(require, "diffview.lib")
+
+  if ok and lib.get_current_view() then
+    vim.cmd("DiffviewClose")
+  else
+    vim.cmd("DiffviewOpen")
+  end
+end, { desc = "Git Diff (Diffview Toggle)" })
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
