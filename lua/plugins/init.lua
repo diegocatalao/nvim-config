0e14a10b29279ab150ce0f26135c234298d0bdd8
Diego Catal <diegohutter@icloud.com>
2025-12-14 22:30:37 -0300

    feat: a nice list of plugins ever

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,28 +1,114 @@
 return {
+  { import = "nvchad.blink.lazyspec" },
+  --
+  -- gitgraph to see the branches like gitkraken
+  -- nice to visualize the connections between feat, dev and main
+  -- i just like to see the git history in a visual way
+  {
+    "isakbm/gitgraph.nvim",
+    dependencies = { "sindrets/diffview.nvim" },
+    keys = {
+      {
+        "<leader>gl",
+        function()
+          require("gitgraph").draw({}, { all = true, max_count = 5000 })
+        end,
+        desc = "Git graph",
+      },
+    },
+    opts = {
+      symbols = {
+        merge_commit = "M",
+        commit = "*",
+      },
+      format = {
+        timestamp = "%H:%M:%S %d-%m-%Y",
+        fields = { "hash", "timestamp", "author", "branch_name", "tag" },
+      },
+    },
+  },
+  --
+  -- gitsigns to see the author and commit in the current line
+  -- i like to see who wrote the shit in the code
+  -- blame is always nice to have
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      require("gitsigns").setup {
+        current_line_blame = true,
+        current_line_blame_opts = {
+          delay = 300,
+          virt_text_pos = "eol",
+        },
+        current_line_blame_formatter = "<author>, <author_time:%d/%m/%Y> - <summary>",
+      }
+    end,
+  },
+  --
+  -- outline to see the symbols in the sidebar
+  -- nice to navigate in large files with functions and classes
+  -- i use <leader>o to toggle this
+  {
+    "hedyhli/outline.nvim",
+    cmd = "Outline",
+    keys = {
+      { "<leader>o", "<cmd>Outline<cr>", desc = "Toggle Outline" },
+    },
+    config = function()
+      require("outline").setup {
+        outline_window = { position = "right" },
+      }
+    end,
+  },
+  --
+  -- virt-column to see the 80 column limit
+  -- i like to keep my code clean and readable
+  -- the colorcolumn native is ugly so i use this
+  {
+    "lukas-reineke/virt-column.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      vim.opt.colorcolumn = "80"
+      require("virt-column").setup()
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        callback = function()
+          vim.api.nvim_set_hl(0, "VirtColumn", { fg = "#3a4248" })
+          vim.api.nvim_set_hl(0, "ColorColumn", { bg = "NONE" })
+        end,
+      })
+      vim.api.nvim_set_hl(0, "VirtColumn", { fg = "#3a4248" })
+      vim.api.nvim_set_hl(0, "ColorColumn", { bg = "NONE" })
+    end,
+  },
+  -- the treesitter plugin, nice for hightlight
+  -- i use this because its nice and funny
+  -- NOTE: complete disabled because the nvchad repo its not
+  -- upgraded with the new version of treesitter in main branch
+  {
+    {
+      "nvim-treesitter/nvim-treesitter",
+      lazy = false,
+      branch = "master",
+    },
+  },
+  -- automate code formating with conform
+  -- i just using this because its confortable for me
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre",
     opts = require "configs.conform",
   },
-
-  -- These are some examples, uncomment them if you want to see them work!
+  -- a generic linter with lsp
+  -- i using this to autocomplete, goto definitions, hover, etc.
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
-
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  -- diffview to see the shit in my code pass
+  -- the best alternative to github interface here
+  -- i just like this plugin, just without reason
+  { "sindrets/diffview.nvim", cmd = "DiffviewOpen" },
 }
