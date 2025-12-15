6454befcff9fe224dc2a25e1a66ac1be763e2df9
Diego Catalão <diego.catalao@gabriel.com.br>
2025-12-15 10:55:42 -0300

    fix: move the default style configuration to options

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,6 @@
 require "nvchad.options"
 
--- add yours here!
+-- this is the main pattern for all projects
+-- but not for C, i just use my own properties
+vim.opt.colorcolumn = "80"
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
